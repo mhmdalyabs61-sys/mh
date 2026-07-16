6076f0e5f56dc74fe4ec3f3d0f3c2826841dd700
mhmdalyabs61-sys <mhmdalyabs61@gmail.com>
2026-07-17 01:27:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -396,6 +396,8 @@ user_histories = {}
 def get_ai_answer(user_id, user_question):
     # تصحيح التنسيق هنا
     if user_id not in user_histories:
+    print([m.name for m in client.models.list()])
+
         user_histories[user_id] = client.chats.create(model="gemini-1.5-flash")
 
     chat = user_histories[user_id]
